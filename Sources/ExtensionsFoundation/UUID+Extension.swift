fd4c9441bc23f7ed64a0d7c87e0540454d7f3f76
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2021-04-29 14:02:21 +0300

    Add an extension for UUID

---
+++ Sources/ExtensionsFoundation/UUID+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  UUID+Extension.swift
+//
+//
+//  Created by Rostyslav Druzhchenko on 29.04.2021.
+//
+
+import Foundation
+
+public extension UUID {
+
+    var toString: String {
+        self.uuidString
+    }
+}
