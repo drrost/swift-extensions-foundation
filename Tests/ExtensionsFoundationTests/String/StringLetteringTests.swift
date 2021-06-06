0fc21e54a7ae0c09d6a354a0ceb63efacfe78b0d
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2021-06-06 22:46:48 +0300

    Add two methods to String extension

---
+++ Tests/ExtensionsFoundationTests/String/StringLetteringTests.swift
@@ -0,0 +1,26 @@
+//
+//  StringLetteringTests.swift
+//
+//
+//  Created by Rostyslav Druzhchenko on 06.06.2021.
+//
+
+import XCTest
+
+@testable import ExtensionsFoundation
+
+class UnitTestTemplateTests: XCTestCase {
+
+    // MARK: - Init tests
+
+    func testCapitalizingFirstLetterInAllWords() {
+        // Given
+        let string = "and others that are found in major traditions"
+
+        // When
+        let result = string.capitalizeAllFirstLetters()
+
+        // Then
+        XCTAssertEqual("And Others That Are Found In Major Traditions", result)
+    }
+}
